7f5a2ca314aeab42d1da65b76a56ed7b8ed1da1f
cesarcuadro <cesarcuadro777@gmail.com>
2022-11-13 19:14:34 -0800

    completed week 3 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,6 +10,18 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
+/*
+Boiler Template
+
+describe("areYouHungry", () => {
+  it("returns eat food or keep coding based on input", () => {
+    expect(areYouHungry("yes")).toEqual("eat food")
+    expect(areYouHungry("no")).toEqual("keep coding")
+  })
+})
+
+*/
+
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
 // a) Create a test with expect statements for each of the variables provided.
@@ -20,10 +32,60 @@ const fibLength1 = 6
 const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+describe("greaterFib", () => {
+  it("returns an array that length containing the numbers of the Fibonacci sequence", () => {
+    expect(greaterFib(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(greaterFib(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+})
+// -> ReferenceError: greaterFib is not defined
+
 // b) Create the function that makes the test pass.
+
+/*
+Create a function that takes in one parameter as an argument
+The argument that the parameter is taking in is a number data type that is greater than 2
+Find and use a method that takes in the fibonacci sequence
+Use the number data type as the length of how many numbers in the fibonacci sequence will be used
+The Array.from() method is a static method that creates a new array. 
+Return the numbers from the fibonacci sequence inside of an array
+*/
+
+const greaterFib = (number) => {
+  let fibNumber = (number - 1) + (number - 2)
+  return Array.from(fibNumber)
+}
+  
+  /*
+
+      - Expected  - 8
+    + Received  + 1
+
+    - Array [
+    -   1,
+    -   1,
+    -   2,
+    -   3,
+    -   5,
+    -   8,
+    - ]
+    + Array []
+
+    Test Suites: 1 failed, 1 total
+Tests:       1 failed, 2 passed, 3 total
+  
+    I am currently stuck at this point. I understand that we need the equation for the fibonacci sequence, but if I take out the Array.from() method, it will only return the number in the sequence. But if I tried inputting predetermined values to start the sequence or using an if statement to confirm that the number argument is larger than two, I either get more numbers than expected or multiple NaN's. 
+    When adding String inside of the Array.from() method, there is one more number than expected on the console instead of the extra being another array.
+
+  */
+  
+
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
+
+//object,values() turns the values of the objects into an array
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -49,7 +111,27 @@ const studyMinutesWeek2 = {
 }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
+describe("studyMinutes", () => {
+  it("returns an array of the values sorted from least to greatest.", () => {
+    expect(studyMinutes(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+    expect(studyMinutes(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
+})
+
+// ReferenceError: studyMinutes is not defined
+
 // b) Create the function that makes the test pass.
+
+//Create a function that takes in one object as a parameter
+//object.values() turns the values of the objects into an array
+//find a way to iterate through the array
+//sort the numbers inside of the array
+//use array.sort() in ascending order
+//array.sort(function(a,b){return a-b}) is the function that takes in two numbers, compares which one is bigger out of the two numbers, then places the bigger number in the b value. This process of sorting is iterated throughout the entire array.
+
+const studyMinutes = (object) => {
+  return Object.values(object).sort(function(a,  b) {return a - b})
+}
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
@@ -57,11 +139,56 @@ const studyMinutesWeek2 = {
 
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
+//Process: [100, 100 - 17 = 83, 83 - 23 = 60, 60 - 9 = 51]
 
 const accountTransactions2 = [250, -89, 100, -96]
 // Expected output: [250, 161, 261, 165]
+//Process: [250, 250 - 89 = 161, 161 + 100 = 261, 261 - 96 = 165]
 
 const accountTransactions3 = []
 // Expected output: []
 
+describe("arithmetic", () => {
+  it("returns an array of the accumulating sum. An empty array should return an empty array", () => {
+    expect(arithmetic(accountTransactions1)).toEqual([100, 83, 60, 51])
+    expect(arithmetic(accountTransactions2)).toEqual([250, 161, 261, 165])
+    expect(arithmetic(accountTransactions3)).toEqual([])
+  })
+})
+
 // b) Create the function that makes the test pass.
+
+/*
+create a function named arithmetic that takes in one array as a parameter
+zero index of the array as the original integer then add the value of the next integer
+Possibly use the .map() method to iterate through the array
+Inside of the .map() method, we will be using the accumulative sum function
+  The accumulative sum function is 
+returns an array of the accumulating sum is when the initial sum is equal to the zero index and value is the input[n]. Once the function is called, the sum is updated with the previous value of output[n-1]. Each time the function is called, the sum will always be called to the zero index.
+  starting at the first index, add the value of the following index
+  the array that is outputted is the same number of the original array
+*/
+
+const arithmetic = (array) => {
+  return array.map((sum => value => sum += value)(0))
+}
+
+/*
+Array [
+        100,
+    -   -17,
+    -   -23,
+    -   -9,
+    +   83,
+    +   60,
+    +   51,
+      ]
+const arithmetic = (array) => {
+  return array.map((sum => value => sum += value)(0))
+}
+The test failed because I put the input of the array that was given instead of the expected output in my testing function. To correct this, I changed the values inside of the .toEqual() method to the actual array for the expected output
+
+Test Suites: 1 failed, 1 total
+Tests:       1 failed, 2 passed, 3 total
+
+*/
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,73 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is a markdown format with the flavor of JavaScript. Both HTML and JSX create the structure of the application you are trying to work with. The main syntax difference between HTML and JSX is using the '<>' tag. Inside of that tag is what will be used to display onto the application.
+For example, we shall use the App.js function in order to show it's application:
 
-Researched answer:
+````JavaScript
+const App = () => {
+    return (
+        <>
+            <h1>Hello World</h1>
+        <>
+    )
+}
+
+````
+
+Researched answer: JSX is a markdown syntax extension to JavaScript. JSX is the building block of React components and what allows React to display as a user interface. The syntax difference between JSX and HTML is that it uses 'className' instead of 'class' because there is already a method in JavaScript named 'class'.
 
 2. What is yarn? What file(s) are modified in a React application when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is a package manager that gives the React application its functionality, as well as Jest. When running the command $yarn create react-app; the terminal will create a local server in order to show the visual aspect of the DOM and the virtual DOM on a web browser
 
-Researched answer:
+Researched answer: Yarn is a package manager that was created by Facebook in 2016. A package manager ensures that dependency files are able to work together. In terms of React, yarn gives the command that creates the file structure and organizes those files for a React app.
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: An anonymous function in JavaScript is when we are not passing in a specific parameter as an argument in a function.
+For example, of an anonymous function: 
 
-Researched answer:
+````JavaScript
+const test = () => {
+    return
+}
+
+//example of a regular function
+
+const test = (parameter) => {
+    return parameter
+}
+````
+
+Researched answer: An anonymous  function is a function that does not have a name associated with it. Anonymous functions are often used as an argument inside of a higher order function.
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: State values are variables that can be used to act dynamically with the user. Props are used as arguments that interact with components of a React app.
 
-Researched answer:
+Researched answer: State values store data that can determine how the components render and behave. To make state dynamic, you would use useState() that changes as the user interacts with the React app. Props pass information from one component to another, the difference is that props cannot be changed.
 
 5. What is the DOM?
 
-Your answer:
+Your answer: The DOM stands for Document Object Model and it is what the user is interacting with as well as tracking the DOM events.
 
-Researched answer:
+Researched answer: The Document Object Model is the visual representation of the code. 
 
 6. STRETCH: Which is the difference between a div and a span?
 
-Your answer:
+Your answer: The difference between a div and a span is that while the div and the span can be used to display on the web browser, the span is a more neutral tag that is specifically used in JSX
 
-Researched answer:
+Researched answer: The difference is that a div is a block level element while a span is an inline level element. A block level element occupies the entire horizontal space of its parent element and the vertical space that is equal to the height of its contents. An inline level element is displayed in a line. They do not force the text after it to form a new line.
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: Object-oriented programming is a computer programming model that organizes software design around data, or objects, rather than functions and logic.
 
-2. Ruby:
+2. Ruby: Ruby is an Object-oriented programming language that is a general purpose, interpreted, dynamically-typed, server-side scripting language
 
-3. Implicit return:
+3. Implicit return: An implicit return means there is no 'return' specified the value of the last line of a method that is automatically returned.
 
-4. Ruby blocks:
+4. Ruby blocks: A Ruby block is an anonymous function that is passed to a method to determine the outcome. Blocks are typically defined with the Ruby keywords 'do' and 'end'.
 
-5. Ruby hashes:
+5. Ruby hashes: A Ruby hash is a dictionary-like container for unique keys and their corresponding values.
